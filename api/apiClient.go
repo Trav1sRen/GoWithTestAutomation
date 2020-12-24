0e1219616bf0f23af2015abb1643b029cb9bd98e
zhuang.a.ren <zhuang.a.ren@rakuten.com>
2020-12-24 17:11:30 +0800

    Try not using log.Fatal

--- api/apiClient.go
+++ api/apiClient.go
@@ -22,18 +22,18 @@ func (*RequestClient) sendSoapRequest(url string, headers map[string]string, req
 
 	res, err := http.Post(url, "application/soap+xml; charset=utf-8", strings.NewReader(requestBody))
 	if err != nil {
-		log.Fatal("Http post error: ", err)
+		log.Print("Http post error: ", err)
 		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		log.Fatalf("SOAP request failed with the status: %d", res.StatusCode)
+		log.Printf("SOAP request failed with the status: %d", res.StatusCode)
 	}
 
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal("Error when reading the response body stream: ", err)
+		log.Print("Error when reading the response body stream: ", err)
 		return err
 	}
 
